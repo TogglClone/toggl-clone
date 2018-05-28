642c7a87bcaa4b859bd3d3cbf5365e0571553e1d
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-05-28 15:15:37 -0600

    basic server added.

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require("dotenv")
+const express = require('express')
+const massive = require('massive')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const Auth0Strategy = require('passport-auth0')
+const cors = require('cors')
+const {
+  CONNECTION_STRING,
+  SESSION_SECRET,
+  SEVER_PORT,
+  DOMAIN,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  CALLBACK_URL
+}
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+massive(CONNECTION_STRING).then(db => {
+  app.set("db", db)
+  app.listen(SERVER_PORT, () => {
+    console.log(`listening on ${SERVER_PORT}`)
+  })
+})
+
+
