62f3f35b4a722dee31e5daf081584f0f16a3b024
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-05-29 09:15:24 -0600

    changes to landing page. styled comps are cool.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react"
+import Landing from "./Components/Landing/Landing"
 import "./App.css"
 
 class App extends Component {
   render() {
-    return <div className="App">app</div>
+    return (
+      <div className="App">
+        <Landing />
+      </div>
+    )
   }
 }
 
