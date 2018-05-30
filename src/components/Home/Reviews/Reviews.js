ff2bd8b457ebe978226ae32c01a7464e623128f4
Brian Macleod <brianmacleod@Brians-MacBook-Pro.local>
2018-05-30 11:17:53 -0600

    brians changes

--- src/components/Home/Reviews/Reviews.js
+++ src/components/Home/Reviews/Reviews.js
@@ -4,6 +4,8 @@ import annika from "./img/annika.jpg"
 import brad from "./img/brad.jpg"
 import tasha from "./img/tasha.jpg"
 import background from "./img/bg.png"
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 export default class Reviews extends Component {
     constructor(){
@@ -21,6 +23,20 @@ export default class Reviews extends Component {
             <ReviewsContainer>
                 <ReviewTitle> Sweet Nothings </ReviewTitle>
                 <ReviewMini> Some client love. </ReviewMini>
+                <CarouselProvider
+                                naturalSlideWidth={100}
+                                naturalSlideHeight={125}
+                                totalSlides={3}
+                                className='carousel'
+                            >
+                                <Slider style={{height:'200px',width:'100%'}}>
+                                <Slide  index=''><img  src='https://files.slack.com/files-pri/T039C2PUY-FAY9WP00J/annika_2x-c866df477ed2e77a109a22d64193df69.jpg' alt="Profile Image" /></Slide>
+                                <Slide  index=''><img src='https://files.slack.com/files-pri/T039C2PUY-FAXJUB4KA/brad_2x-b55bf1cded49d029b980097e2c438d12.jpg' alt="Profile Image" /></Slide>
+                                <Slide  index=''><img src='https://files.slack.com/files-pri/T039C2PUY-FAY6YFNUT/tasha_2x-78c9d2020a6a35b7f39a6e60915f0b26.jpg' alt="Profile Image" /></Slide>
+                                </Slider>
+                                <ButtonBack >Back</ButtonBack>
+                                <ButtonNext>Next</ButtonNext>
+                </CarouselProvider>
                 {/* <ReviewContent>  */}
                 {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.8 69.6"><path d="M23.5 68.1V63a5.6 5.6 0 0 1 5.6-5.6h1a5.6 5.6 0 0 0 5.6-5.6 5.6 5.6 0 0 0-5.6-5.6h-13a5.6 5.6 0 0 1-5.6-5.6 5.6 5.6 0 0 1 5.6-5.6h21.5a5.6 5.6 0 0 0 5.6-5.6 5.6 5.6 0 0 0-5.6-5.6H7.1a5.6 5.6 0 0 1-5.6-5.6 5.6 5.6 0 0 1 5.6-5.6h10.8A5.6 5.6 0 0 0 23.5 7V1.8" fill="none" stroke="#ffacba" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" style="stroke-dashoffset: 0px;"></path></svg> */}
             </ReviewsContainer>
