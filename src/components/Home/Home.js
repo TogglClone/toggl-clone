1a7af2cbf236fefab74e1512e586dabfb0c815a7
bygoneunicorn <nathansoccer.jones@gmail.com>
2018-05-29 22:28:27 -0600

    timer nav and icons and css skeleton setup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
-import Landing from "../Landing/Landing"
+import Landing from "./Landing/Landing"
 
 class Home extends Component {
   render() {
