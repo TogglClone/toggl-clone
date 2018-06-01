5132b6352b1e2d84f2f60ce19137948e5b4fe559
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-06-01 12:56:43 -0600

    some big changes. adding burger menu. got to display. burger stuff... will need to add the animation and organize the page a big. removed login. added to menu.

---
+++ src/components/Home/Landing/SignUpBottomSvg/SignUpBottomSvg.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react"
+import styled from "styled-components"
+
+export default class BottomLoginSvg extends Component {
+  render() {
+    return (
+      <div style={svgSize}>
+        <svg>
+          <path
+            class="sign-up-line sign-up-line--4"
+            d="M6,18-2.3-.4"
+            transform="translate(4.3 13.4)"
+            style={svgSize}
+            fill="none"
+            stroke-width="4"
+          />
+          <path
+            class="sign-up-line sign-up-line--5"
+            d="M22,16.1V-11.4"
+            transform="translate(4.3 13.4)"
+            style={svgSize}
+            fill="none"
+            stroke-width="4"
+          />
+          <path
+            class="sign-up-line sign-up-line--6"
+            d="M37.9,18,46.2-1.4"
+            transform="translate(4.3 13.4)"
+            style={svgSize}
+            fill="none"
+            stroke-width="4"
+          />
+        </svg>
+      </div>
+    )
+  }
+}
+
+const svgStyle = {
+  strokeDashoffset: "0",
+  strokeMiterlimit: "4",
+  stroke: "black",
+  strokeLinecap: "round"
+}
+
+const svgSize = {
+  width: "1rem",
+  height: "1rem"
+}
